690d4f549843123c20f7310e0ad4f0d3aef60dd2
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-03-24 19:29:44 +0000

    setting up all my hooks for my loginpage

--- instagram-react-frontend/src/pages/login.js
+++ instagram-react-frontend/src/pages/login.js
@@ -1,4 +1,26 @@
-export default function Login(){
+import { useContext, useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import FirebaseContext from "../context/firebase"
+
+export default function Login() {
+	const history = useHistory();
+	const { firebase } = useContext(FirebaseContext);
+
+	const [emailAddress, setEmailAddress] = useState("");
+	const [password, setPassword] = useState("");
+
+	const [error, setError] = useState("");
+
+	const isInvalid = password === "" || emailAddress === "";
+
+	const handleLogin = () => {
+
+	};
+
+	useEffect(() => {
+		document.title = "Login - Instagram";
+	}, [])
+
 	return (
 		<div>
 			<h1>Login Page</h1>
