d8bcbb9730c685cd6e04ca780c5e532bb0a9f93e
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-03-24 21:22:43 +0000

    configured tailwind config file to add custom colours

--- instagram-react-frontend/tailwind.config.js
+++ instagram-react-frontend/tailwind.config.js
@@ -1,9 +1,32 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
-  theme: {
-    extend: {},
+  future: {
+    removeDeprecatedGapUtilities: true
   },
-  plugins: [],
-}
+  purge: {
+    content: ['./src/**/*.js', './src/**/**/*.js']
+  },
+  theme: {
+    fill: (theme) => ({
+      red: theme('colors.red.primary')
+    }),
+    colors: {
+      white: '#ffffff',
+      blue: {
+        medium: '#005c98'
+      },
+      black: {
+        light: '#262626',
+        faded: '#00000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      },
+      red: {
+        primary: '#ed4956'
+      }
+    }
+  },
+  variants: {}
+};
