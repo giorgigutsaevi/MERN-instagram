5510c79de12d7025d8e1513b38c2daf4f30fa12a
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-03-25 11:20:35 +0000

    Added some SVG icons for home and logout Link components

--- instagram-react-frontend/src/components/Header.js
+++ instagram-react-frontend/src/components/Header.js
@@ -6,7 +6,7 @@ import FirebaseContext from "../context/firebase";
 const Header = () => {
 
 	const { firebase } = useContext(FirebaseContext)
-	const user = null;
+	const user = 1;
 
 	return (
 		<header className="h-16 bg-white border-b mb-8">
@@ -23,9 +23,22 @@ const Header = () => {
 					</div>
 					<div className="text-gray text-center flex items-center align-items">
 						{user ? (
-							<>
+							<> 
 								<Link to="/">
-									<p>Home</p>
+									<svg
+										className="w-8 mr-6 text-black-light cursor-pointer"
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke="currentColor"
+									>
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth={2}
+											d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+										/>
+									</svg>
 								</Link>
 
 								<button
@@ -34,7 +47,20 @@ const Header = () => {
 									onClick={() => firebase.auth().signOut()}
 
 								>
-									Sign out
+									<svg
+										className="w-8 mr-6 text-black-light cursor-pointer"
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke="currentColor"
+									>
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth={2}
+											d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+										/>
+									</svg>
 								</button>
 								<div className="flex items-center cursor-pointer">
 									<Link to={`/p/${user.displayName}`}>
